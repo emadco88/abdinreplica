a5da78004c1d9c1a24e405d04544a77926fbf961
emadco88 <emadco88@gmail.com>
2024-08-25 16:49:45 +0300

    ab_inventory_adjust/ rename HelperFunctions to Helper

--- ab_inventory_adjust/static/src/js/odoo_barcode_one2many.js
+++ ab_inventory_adjust/static/src/js/odoo_barcode_one2many.js
@@ -2,7 +2,7 @@ odoo.define('ab_inventory_adjust.barcode_reader', function (require) {
     "use strict";
 
     const FormController = require('web.FormController');
-    const helperFunctions = require('ab_inventory_adjust.helper_functions');
+    const Helper = require('ab_inventory_adjust.helper_functions');
 
     FormController.include({
         start: function () {
@@ -13,20 +13,20 @@ odoo.define('ab_inventory_adjust.barcode_reader', function (require) {
 
             this.$el.on('keydown', async function (event) {
 
-                if (event.key === helperFunctions.PREFIX && !PREFIXPressed) {
+                if (event.key === Helper.PREFIX && !PREFIXPressed) {
                     PREFIXPressed = true;
                     keysSent = false;
                     event.preventDefault(); // Prevent default PREFIX behavior
 
-                    const barcode = await helperFunctions.collectBarcodeKeys();
+                    const barcode = await Helper.collectBarcodeKeys();
                     PREFIXPressed = false; // Reset PREFIX flag after processing
 
-                    await helperFunctions.addLineIfNeeded();
+                    await Helper.addLineIfNeeded();
 
                     document.activeElement.value = barcode;
                     document.activeElement.dispatchEvent(new Event('input', {bubbles: true}));
 
-                    const itemsCount = await helperFunctions.fetchDropdownItems(barcode);
+                    const itemsCount = await Helper.fetchDropdownItems(barcode);
 
                     if (itemsCount !== 1) return;
 
