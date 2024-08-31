9981bd05cad74f21f4bd14d8e7643af026f462d6
emadco88 <emadco88@gmail.com>
2024-08-31 04:48:01 +0300

    ab_odoo_update/ UPDs 24

--- ab_odoo_update/restart_odoo_server.py
+++ ab_odoo_update/restart_odoo_server.py
@@ -21,12 +21,12 @@ def start_odoo_service():
 if __name__ == '__main__':
     # Run stop_odoo_service in a separate Python instance
     # subprocess.run(['D:\\odoo\\python\\python.exe', 'stop_service.py'], check=True)
-    subprocess.run(f'sc stop {service_name} && sc start {service_name}', shell=True, check=True)
+    subprocess.run(f'sc stop {service_name} && timeout /t 5 && sc start {service_name}', shell=True, check=True)
     # Wait until the service is stopped
-    for _ in range(5):
-        if not is_service_stopped():
-            time.sleep(1)  # Check every second
-        else:
-            break
-
-    start_odoo_service()
+    # for _ in range(5):
+    #     if not is_service_stopped():
+    #         time.sleep(1)  # Check every second
+    #     else:
+    #         break
+    #
+    # start_odoo_service()
