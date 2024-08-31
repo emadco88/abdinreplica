a341791a2199b628b7a9d532af03a9083c10e2ba
emadco88 <emadco88@gmail.com>
2024-08-31 05:10:48 +0300

    ab_odoo_update/ UPDs not working

--- ab_odoo_update/restart_odoo_server.py
+++ ab_odoo_update/restart_odoo_server.py
@@ -3,4 +3,4 @@ import subprocess
 service_name = 'odoo-server-15.0'
 
 if __name__ == '__main__':
-    subprocess.run(f'sc stop {service_name} && timeout /t 5 && sc start {service_name}', shell=True, check=True)
+    subprocess.run(f'sc restart {service_name}', shell=True, check=True)
