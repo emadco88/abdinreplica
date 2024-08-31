f3a1dbacd2b43f245f9f558f5280de3b1067e3d2
emadco88 <emadco88@gmail.com>
2024-08-31 03:33:40 +0300

    ab_odoo_update/ UPDs 11

--- ab_odoo_update/restart_odoo_server.py
+++ ab_odoo_update/restart_odoo_server.py
@@ -1,15 +1,20 @@
 import subprocess
 import time
 
+service_name = 'odoo-server-15.0'
 
-def restart_odoo_service():
-    service_name = 'odoo-server-15.0'
 
+def stop_odoo_service():
     # Stop the Odoo service
-    subprocess.run(f'sc stop {service_name}', shell=True, check=True)
-    time.sleep(2)  # Wait for 1 second before checking again
-    subprocess.run(f'sc start {service_name}', shell=True, check=True)
+    return subprocess.run(f'sc stop {service_name}', shell=True, check=True)
+
+
+def start_odoo_service():
+    # Stop the Odoo service
+    return subprocess.run(f'sc start {service_name}', shell=True, check=True)
 
 
 if __name__ == '__main__':
-    restart_odoo_service()
+    stop_odoo_service()
+    time.sleep(2)
+    start_odoo_service()
