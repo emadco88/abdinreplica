a234f3b65181b9e19a6ae49a6e032e5acc95b16a
emadco88 <emadco88@gmail.com>
2024-08-31 05:01:23 +0300

    ab_odoo_update/ UPDs 24

--- ab_odoo_update/restart_odoo_server.py
+++ ab_odoo_update/restart_odoo_server.py
@@ -5,5 +5,5 @@ service_name = 'odoo-server-15.0'
 if __name__ == '__main__':
     # subprocess.run(f'sc stop {service_name} && timeout /t 5 && sc start {service_name}', shell=True, check=True)
 
-    batch_file_path = 'restart_odoo_server'
+    batch_file_path = 'restart_odoo_server.bat'
     subprocess.run(f'start "" "{batch_file_path}"', shell=True)
