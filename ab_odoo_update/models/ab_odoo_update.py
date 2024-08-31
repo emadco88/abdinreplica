7aaf0b8471ba0f63887d3f1a06dd3f9bf1cc3e64
emadco88 <emadco88@gmail.com>
2024-08-31 03:20:40 +0300

    ab_odoo_update/ UPDs 8

--- ab_odoo_update/models/ab_odoo_update.py
+++ ab_odoo_update/models/ab_odoo_update.py
@@ -14,53 +14,43 @@ class OdooServerControl(models.AbstractModel):
 
     @api.model
     def git_pull(self):
-        try:
-            # Step 1: Use tools.config to get the addons_path from the current Odoo configuration
-            addons_path = tools.config['addons_path']
+        # Step 1: Use tools.config to get the addons_path from the current Odoo configuration
+        addons_path = tools.config['addons_path']
 
-            # Assuming the odoo_replica_addons is one of the directories in addons_path
-            replica_addons_path = None
-            for path in addons_path.split(','):
-                if 'replica_addons' in path:
-                    replica_addons_path = path.strip()
-                    break
+        # Assuming the odoo_replica_addons is one of the directories in addons_path
+        replica_addons_path = None
+        for path in addons_path.split(','):
+            if 'replica_addons' in path:
+                replica_addons_path = path.strip()
+                break
 
-            if not replica_addons_path:
-                raise Exception('odoo_replica_addons path not found in addons_path')
+        if not replica_addons_path:
+            raise UserError('odoo_replica_addons path not found in addons_path')
 
-            # Step 2: Perform a git pull on the replica_addons repository
-            os.chdir(replica_addons_path)
-            subprocess.run(['git', 'pull'], check=True)
-
-            return {'status': 'success', 'message': 'Git Pull new updates successfully.'}
-        except Exception as e:
-            return {'status': 'error', 'message': str(e)}
+        # Step 2: Perform a git pull on the replica_addons repository
+        os.chdir(replica_addons_path)
+        subprocess.run(['git', 'pull'], check=True)
 
     @api.model
     def restart_odoo_server(self):
-        try:
-            script_path = ''
-            addons_path = tools.config['addons_path']
-            for path in addons_path.split(','):
-                if 'replica_addons' in path:
-                    script_path = path.strip()
-                    break
+        script_path = ''
+        addons_path = tools.config['addons_path']
+        for path in addons_path.split(','):
+            if 'replica_addons' in path:
+                script_path = path.strip()
+                break
 
-            # Step 1: Construct the path to the external script
-            script_path = os.path.join(script_path, 'ab_odoo_update', 'restart_odoo_server.py')
-            _logger.info('####################')
-            _logger.info(f'script_path: {script_path}')
-            # Step 2: Ensure the script exists
-            if not os.path.isfile(script_path):
-                raise UserError(f"Script not found: {script_path}")
+        # Step 1: Construct the path to the external script
+        script_path = os.path.join(script_path, 'ab_odoo_update', 'restart_odoo_server.py')
+        _logger.info('####################')
+        _logger.info(f'script_path: {script_path}')
+        # Step 2: Ensure the script exists
+        if not os.path.isfile(script_path):
+            raise UserError(f"Script not found: {script_path}")
 
-            _logger.info('******************')
-            _logger.info(f'script_path: {script_path}')
+        _logger.info('******************')
+        _logger.info(f'script_path: {script_path}')
 
-            # Step 3: Use runas to run the script as admin
-            command = f'runas /user:Administrator "python {script_path}"'
-            subprocess.Popen(command, shell=True)
-
-            return {'status': 'success', 'message': 'Restart command issued successfully.'}
-        except Exception as e:
-            return {'status': 'error', 'message': str(e)}
+        # Step 3: Use runas to run the script as admin
+        command = f'runas /user:Administrator "python {script_path}"'
+        subprocess.Popen(command, shell=True)
