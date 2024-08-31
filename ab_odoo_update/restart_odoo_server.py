138c672458a3d089e3ec0df7537972b42e63b0a4
emadco88 <emadco88@gmail.com>
2024-08-31 04:26:49 +0300

    ab_odoo_update/ UPDs 23

--- ab_odoo_update/restart_odoo_server.py
+++ ab_odoo_update/restart_odoo_server.py
@@ -1,23 +1,34 @@
 import subprocess
 import time
-import logging
-
-_logger = logging.getLogger(__name__)
+import re
 
 service_name = 'odoo-server-15.0'
 
 
 def stop_odoo_service():
-    # Stop the Odoo service
-    return subprocess.run(f'sc stop {service_name}', shell=True, check=True)
+    """Stop the Odoo service."""
+    subprocess.run(f'sc stop {service_name}', shell=True, check=True)
+
+
+def is_service_stopped():
+    """Check if the Odoo service is stopped."""
+    result = subprocess.run(f'sc query {service_name}', shell=True, capture_output=True, text=True)
+
+    # Use regex to check the service state
+    return re.search(r'STATE\s*:\s*1\s*STOPPED', result.stdout) is not None
 
 
 def start_odoo_service():
-    # Stop the Odoo service
-    return subprocess.run(f'sc start {service_name}', shell=True, check=True)
+    """Start the Odoo service."""
+    subprocess.run(f'sc start {service_name}', shell=True, check=True)
 
 
 if __name__ == '__main__':
     stop_odoo_service()
-    time.sleep(2)
+
+    # Wait until the service is stopped
+    for _ in range(10):
+        if not is_service_stopped():
+            time.sleep(1)  # Check every second
+
     start_odoo_service()
