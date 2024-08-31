6dce765b190c7287d41b2f97d12db614a25083d5
emadco88 <emadco88@gmail.com>
2024-08-31 05:28:04 +0300

    ab_odoo_update/ UPDs not working

--- ab_odoo_update/restart_odoo_server.py
+++ ab_odoo_update/restart_odoo_server.py
@@ -3,4 +3,4 @@ import subprocess
 service_name = 'odoo-server-15.0'
 
 if __name__ == '__main__':
-    subprocess.run(f'net stop odoo-server-15.0  && net start odoo-server-15.0', shell=True, check=True)
+    subprocess.run(f'net start odoo-server-15.0', shell=True, check=True)
