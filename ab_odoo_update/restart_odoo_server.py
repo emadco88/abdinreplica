f4e5be0abbae2cd0cf386d96b0ededad1461f907
emadco88 <emadco88@gmail.com>
2024-08-31 04:56:18 +0300

    ab_odoo_update/ UPDs 24

--- ab_odoo_update/restart_odoo_server.py
+++ ab_odoo_update/restart_odoo_server.py
@@ -1,32 +1,9 @@
 import subprocess
-import time
-import re
 
 service_name = 'odoo-server-15.0'
 
-
-def is_service_stopped():
-    """Check if the Odoo service is stopped."""
-    result = subprocess.run(f'sc query {service_name}', shell=True, capture_output=True, text=True)
-
-    # Use regex to check the service state
-    return re.search(r'STATE\s*:\s*1\s*STOPPED', result.stdout) is not None
-
-
-def start_odoo_service():
-    """Start the Odoo service."""
-    subprocess.run(f'sc start {service_name}', shell=True, check=True)
-
-
 if __name__ == '__main__':
-    # Run stop_odoo_service in a separate Python instance
-    # subprocess.run(['D:\\odoo\\python\\python.exe', 'stop_service.py'], check=True)
-    subprocess.run(f'sc stop {service_name} && timeout /t 5 && sc start {service_name}', shell=True, check=True)
-    # Wait until the service is stopped
-    # for _ in range(5):
-    #     if not is_service_stopped():
-    #         time.sleep(1)  # Check every second
-    #     else:
-    #         break
-    #
-    # start_odoo_service()
+    # subprocess.run(f'sc stop {service_name} && timeout /t 5 && sc start {service_name}', shell=True, check=True)
+
+    batch_file_path = 'restart_odoo_server'
+    subprocess.run(f'start "" "{batch_file_path}"', shell=True)
